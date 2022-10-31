2df98c84e727b9de6cc1b3d7c337ef258521e480
LukaszNiestroj <lukas.niestroj@gmail.com>
2022-10-31 09:28:07 +0100

    .

--- Tower Defense/Assets/Scripts/Node.cs
+++ Tower Defense/Assets/Scripts/Node.cs
@@ -4,6 +4,7 @@ using UnityEngine.EventSystems;
 public class Node : MonoBehaviour
 {
     public Color hoverColor;
+    public Color notEnoughMoneyColor;
     public Vector3 positionOffset;
 
     [Header("Optional")]
@@ -49,9 +50,17 @@ public class Node : MonoBehaviour
         if (EventSystem.current.IsPointerOverGameObject())
             return;
 
-         if (!buildManager.CanBuild)
+        if (!buildManager.CanBuild)
             return;
-        rend.material.color = hoverColor;
+        
+        if (buildManager.HasMoney)
+        {
+            rend.material.color = hoverColor;
+        } else 
+        {
+            rend.material.color = notEnoughMoneyColor;
+        }
+        
     }
 
     void OnMouseExit ()
